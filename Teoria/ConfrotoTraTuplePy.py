8c6fce6f59300528ffc5a14428c370d1059d5c43
Heisenberg99-pr <stefanocavalieri38@gmail.com>
2026-03-05 11:47:39 +0100

    Update files

--- Teoria/ConfrotoTraTuplePy.py
+++ Teoria/ConfrotoTraTuplePy.py
@@ -1,5 +1,9 @@
 
 """
+Confronto tra Tuple
+Le tuple sono strutture dati immutabili che possono contenere elementi di diversi tipi.
+In Python, è possibile confrontare le tuple utilizzando gli operatori di confronto standard
+
 In seguito una dimostrazione di confronto tra tuple, con tutte le situazioni
 """
 
@@ -9,14 +13,3 @@ print( (10, 12) < (10, 1234567) ) # Nel caso i primi valori uguali confronta i s
 print( (10, 12) < (10, 12, 0) ) # Nel caso una delle due tuple finisce prima viene confrontata la lunghezza
 print( (10, "uno") < (10, "due") ) # Confronta "uno" < "due" e determina in base al valore ascii
 
-"""
-In seguito una dimostrazione di confronto tra tuple, con tutte le situazioni
-"""
-
-print( (10, 12) < (8, 1234567) ) # confronta i primi elementi
-print( (10, 12) < (18, 1234567) )
-print( (10, 12) < (10, 1234567) ) # Nel caso i primi valori uguali confronta i secondi
-print( (10, 12) < (10, 12, 0) ) # Nel caso una delle due tuple finisce prima viene confrontata la lunghezza
-print( (10, "uno") < (10, "due") ) # Confronta "uno" < "due" e determina in base al valore ascii
-
-print( (11, 2) < (10, "due") ) # X errore: int e str non sono confrotabili 
--- Teoria/FunzioneSortPy.py
+++ Teoria/FunzioneSortPy.py
@@ -8,15 +8,4 @@ il parametro key ha lo stesso significato che nella funzione merge
 a = ["zero", "uno", "due", "tre", "quattro", "cinque"]
 b = sorted(a, key=lambda x: (len(x), x))
 print(a)
-
-
-"""
-La funzione sort permette di ordinare una sequenza ritornando una nuova lista ordiata
-il parametro key ha lo stesso significato che nella funzione merge
-"""
-
-a = ["zero", "uno", "due", "tre", "quattro", "cinque"]
-b = sorted(a, key=lambda x: (len(x), x))
-print(a)
-
 print(b)
