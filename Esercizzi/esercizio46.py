84aa30fd670a816c80a14a2abc3b32380ac98add
Heisenberg99-pr <stefanocavalieri38@gmail.com>
2026-06-04 11:55:25 +0200

    Add deep_mean function to calculate the mean of numbers in a nested list

---
+++ Esercizzi/esercizio46.py
@@ -0,0 +1,28 @@
+"""
+Creare una funzione deep_mean, che calcoli la media dei numeri all'interno di una lista
+
+es. lista = [1,2,3] ==> result = 1+2+3 / count = 3, in questo caso 
+
+Allora scandire i singoli elementi della lista in un contatore [count] 
+
+Ho una lista = [1,[2,3]] ==> 1 non è lista e allora somma = 1
+
+"""
+
+def deep_mean(lista, somma = 0,count = 0):
+    for elem in lista:
+        if type(elem)==list:
+            sotto_somma, sotto_count = deep_mean(elem)
+            somma += sotto_somma
+            count += sotto_count
+        else:
+            somma+= elem;
+            count+=1; 
+           
+    return somma,count
+    
+lista = [1,[2,3]]
+tot, count = deep_mean(lista)
+media = tot/count
+print(f"{media}")
+
